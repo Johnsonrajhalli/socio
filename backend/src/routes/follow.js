b650b6faa457fb93a2d5030fd7c92037ab4d6a54
BanothuArpitha <banothuarpitha2006@gmail.com>
2025-12-08 14:27:05 +0530

    updated frontend

--- backend/src/routes/follow.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const followCtrl = require('../controllers/followControllers');
-
-router.post('/:id/toggle', auth, followCtrl.toggleFollow);
-
-module.exports = router;
--- backend/src/routes/likes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const likeCtrl = require('../controllers/likeControllers');
-
-router.post('/:postId/toggle', auth, likeCtrl.toggleLike);
-
-module.exports = router;
